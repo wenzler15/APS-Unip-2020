785ca5522a0e4ef543cc0ce6d34919efc42d7290
Gabriel Stefanoni Viestel <GSViestel@raiadrogasil.com>
2020-05-24 15:19:45 -0300

    contato e sobre

--- src/app/components/contatos/contatos.component.ts
+++ src/app/components/contatos/contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contatos',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contatos.component.css']
 })
 export class ContatosComponent implements OnInit {
+  formLogin : FormGroup;
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
+  private createForm(): FormGroup{
+    return this.fb.group({
+
+      nome:new FormControl(null, Validators.compose([
+        Validators.required
+        
+      ])),
+      email: new FormControl(null, Validators.compose([
+      Validators.required,
+      Validators.email
+    ]))
+    })
+}
 
   ngOnInit(): void {
+    this.formLogin = this.createForm();
   }
 
+  // fazerLogin(){
+  //   // console.log(this.login);
+  //   this.loginAuthService.verificarUsuario(this.formLogin).subscribe((data: string[] )=>{
+  //     this.loginAuthService.fazerLogin(data);
+  //   });
+  // }
 }
